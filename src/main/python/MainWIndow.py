891da0c6c092b76551332020705ba2556133a66d
annin401 <cando85@icloud.com>
2019-05-24 21:20:58 +0900

    Typo correction

--- src/main/python/MainWIndow.py
+++ src/main/python/MainWIndow.py
@@ -1,6 +1,6 @@
-from PyQt5 import QWidgets
+from PyQt5 import QtWidgets
 
-class MainWindow( QWidgets.QMainWindow ):
+class MainWindow( QtWidgets.QMainWindow ):
 
     def __init__(self):
         super().__init__()
