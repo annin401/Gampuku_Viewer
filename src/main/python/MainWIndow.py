1475f71c8f92094aa47cebf8faf006bfd8d37368
annin401 <cando85@icloud.com>
2019-05-24 21:14:31 +0900

    make MainWindow

---
+++ src/main/python/MainWIndow.py
@@ -0,0 +1,22 @@
+from PyQt5 import QWidgets
+
+class MainWindow( QWidgets.QMainWindow ):
+
+    def __init__(self):
+        super().__init__()
+
+        self.image_path = None # TODO クラスを作る
+        self.image_view_scene = None # TODO クラスを作る
+        self.image_viewer = None # TODO QGrapphicsView を入れる
+
+        self.initUI()
+
+    def initUI(self):
+
+        self.contextMenu =  None # TODO コンテキストメニュー を作って入れる
+
+    def set_file(self, filepath):
+        pass # TODO
+
+    def start_slideshow(self):
+        pass # TODO
