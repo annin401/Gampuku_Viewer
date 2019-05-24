1137b9c3d7c8644d91a7996f49d4c19d7a135ddc
annin401 <cando85@icloud.com>
2019-05-24 21:22:35 +0900

    add MainWindow.py to main.py

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,12 +1,16 @@
 import sys
 from fbs_runtime.application_context import ApplicationContext
+from MainWindow import MainWindow
 
 class AppContext(ApplicationContext):
 
     def __init__(self):
         super().__init__()
 
+        self.main_window = MainWindow()
+
     def run(self):
+        self.main_window.show()
         return self.app.exec_()
     
 if __name__ == '__main__':
