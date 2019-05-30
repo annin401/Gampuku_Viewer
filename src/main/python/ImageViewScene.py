3ad4e51327497083e2e6ad46c3c09afd52f55f89
annin401 <cando85@icloud.com>
2019-05-30 18:53:26 +0900

    make fitImage in ImageViewScene

--- src/main/python/ImageViewScene.py
+++ src/main/python/ImageViewScene.py
@@ -12,7 +12,7 @@ class ImageViewScene( QtWidgets.QGraphicsScene ):
     def set_file(self, filepath: str) -> None :
 
         # 既にシーンにPixmapアイテムがある場合は削除する。
-        if self.__imageItejm:
+        if self.__imageItem:
             self.removeItem(self.__imageItem)
 
         # イメージを所得
@@ -29,8 +29,60 @@ class ImageViewScene( QtWidgets.QGraphicsScene ):
 
     def fit_image(self):
         
-        if not self.imageItem:
+        if not self.__imageItem:
             return
 
         # sceneのサイズを指定
         self.setSceneRect(0.0, 0.0, self.__secene_width, self.__scene_height)
+
+        # イメージの元の大きさを持つRectオブジェクト
+        boundingRect = self.__imageItem.boundingRect()
+        # シーンの現在の大きさを持つRectオブジェクト
+        sceneRect = self.sceneRect()
+
+        # それぞれの辺の比を求める
+        itemAspectRatio = boundingRect.width() / boundingRect.height()
+        sceneAspectRatio = sceneRect.width() / sceneRect.height()
+
+        if itemAspectRatio >= sceneAspectRatio:
+            # 横幅に合うようにフィット
+            scaleRatio = sceneRect.width() / boundingRect.width()
+        else:
+            # 縦幅に合うようにフィット
+            scaleRatio = sceneRect.height() / boundingRect.height()
+
+        # 最終的にイメージのアイテムに適応するためのオブジェクト
+        transform = QtGui.QTransform()
+
+        # アスペクト比からスケール比を割り出しTransformオブジェクトに適応
+        transform.scale(scaleRatio, scaleRatio)
+
+        #変換されたオブジェクトをイメージに適応
+        self.__imageItem.setTransform(transform)
+
+
+        # 中央に配置する処理
+        # ほんとは違うメソッドにした方が良いが再計算をしたくないため分けてない
+        if itemAspectRatio >= sceneAspectRatio:
+            # 横幅に合わせた場合
+            # 変形後の画像の横幅を所得
+            height_after_transforme = boundingRect.height() * scaleRatio
+
+            # 中央の座標を所得
+            # viewの縦の長さ - 画像　/ 2　で中心の座標を求めてる
+            centerPos = (self.height() / 2) - (height_after_transforme / 2)
+
+            # 画像を移動
+            self.__imageItem.setPos(0, centerPos)
+
+        else:
+            # 縦幅に合わせた場合
+            # 変形後の画像の横幅を所得
+            width_after_transforme = boundingRect.width() * scaleRatio
+
+            # 中央の座標を所得
+            # viewの横の長さ - 画像　/ 2　で中心の座標を求めてる
+            centerPos = (self.width() / 2) - (width_after_transforme / 2)
+
+            # 画像を移動
+            self.__imageItem.setPos(centerPos, 0)
