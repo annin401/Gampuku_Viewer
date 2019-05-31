73d327cc299280eecb309b8d2260e12d1359cf4a
annin401 <cando85@icloud.com>
2019-05-31 12:49:40 +0900

    make ImagePaths possible to slideshow

--- src/main/python/ImageViewer.py
+++ src/main/python/ImageViewer.py
@@ -18,6 +18,14 @@ class ImageViewer( QtWidgets.QGraphicsView ):
                 self.set_imageViewer()
                 self.init_menu()
 
+                # for slideshow
+                self.timer = QtCore.QTimer()
+                self.timer.timeout.connect(self.update) 
+                self.update_interval = 2000 # ミリ秒
+
+                # for self.update
+                self.path_index = 0
+
         def set_imageViewer(self):
                 # フラグセット
                 self.setWindowFlags(QtCore.Qt.CustomizeWindowHint) # タイトルバーを消す
@@ -39,13 +47,7 @@ class ImageViewer( QtWidgets.QGraphicsView ):
             pass # TODO メニューを作る
 
 
-        def resize_event(self, event):
-             # ビューをリサイズ時にシーンの矩形を更新する
-             super().resizeEvent( event )   
-             self.scene().setSceneRect(QtCore.QRectF(self.rect()))
-
-
-        def set_ImagePaths(self):
+        def show_set_Dialog(self):
 
             # ファイルダイアログを表示
             dirpath = QtWidgets.QFileDialog.getExistingDirectory(self,
@@ -56,15 +58,39 @@ class ImageViewer( QtWidgets.QGraphicsView ):
             self.image_paths.make_list(dirpath)
 
         def start_slideshow(self):
-            pass # TODO 
-            # ビューが持つシーンにファイルパスを渡す
-            # self.scene().setFile(filepath)
 
-# for debug
-# app = QtWidgets.QApplication([])
-# i = ImageViewer()
-# i.show()
-# i.set_ImagePaths()
-# print(i.image_paths.path_list)
-# import sys
-# sys.exit( app.exec_() )
+            if not self.image_paths:
+                return 
+
+            # 画像更新をする関数を呼び出すタイマーをスタートする
+            self.timer.start(self.update_interval)
+
+            # 最初に表示する画像をセットする
+            self.update()
+
+
+        def update(self):
+
+            # インデックスが最後まで到達したら最初に戻す
+            if self.path_index == len(self.image_paths):
+                self.path_index = 0
+
+            # 画像をセットする
+            self.scene().set_file( self.image_paths[self.path_index] )
+
+            # インデックスを更新
+            self.path_index += 1 
+
+        def resize_event(self, event):
+             # ビューをリサイズ時にシーンの矩形を更新する
+             super().resizeEvent( event )   
+             self.scene().setSceneRect(QtCore.QRectF(self.rect()))
+
+#for debug 
+app = QtWidgets.QApplication([])
+i = ImageViewer()
+i.show()
+i.show_set_Dialog()
+i.start_slideshow()
+import sys
+sys.exit( app.exec_() )
